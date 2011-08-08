f225f206a3ff1cec82997da1f95109a909d54d23
tehcodedninja <tehcodedninja@gmail.com>
2011-08-07 20:26:02 -0400

    Fixed merge

--- applets/twitter/twiml.php
+++ applets/twitter/twiml.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $name = AppletInstance::getValue('name');
 $hash = AppletInstance::getValue('hash');
@@ -35,36 +34,8 @@ if(AppletInstance::getFlowType() == 'voice'){
 		$response->addRedirect($next);
 }
 else
+{
 	$response->addSms($OneTweet);
-
-=======
-<?php
-$name = AppletInstance::getValue('name');
-
-$response = new Response();
-
-if(!empty($name)) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'http://api.twitter.com/1/users/show.json?screen_name=' . $name);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	$tweet = json_decode(curl_exec($ch));
-	curl_close($ch);
-
-	if(AppletInstance::getFlowType() == 'voice') {
-		$response->addSay($tweet->status->text);
-		$next = AppletInstance::getDropZoneUrl('next');
-		if(!empty($next))
-			$response->addRedirect($next);
-	}
-	else
-		$response->addSms($tweet->status->text);
 }
 
-<<<<<<< HEAD
->>>>>>> 172960aac0199dd7059f9b18661c258b42652a30
 $response->Respond();
-=======
-$response->Respond();
->>>>>>> 4384673b2ad3a3330b70e92274254521d1c85ce2
